0b1f5afa146e2c2cb8c7a4708ae4e9c18bcf00ab
Callum Jones <call_jones2@hotmail.com>
2026-03-25 19:48:14 +0100

    added initial routes and updated plan

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <Routes>
       <Route path="/" element="" />
+      <Route path="/products" element="">
+        <Route path=":id" element="" />
+      </Route>
+      <Route path="/about" element="" />
+      <Route path="/checkout" element="" />
+      <Route path="/account" element="" />
     </Routes>
   )
 }
