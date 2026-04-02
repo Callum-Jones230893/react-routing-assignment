e1e11b4fb9d58ed80e404d86d7cca3c3ae2aca38
Callum Jones <call_jones2@hotmail.com>
2026-04-02 16:27:13 +0200

    added images

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -6,24 +6,26 @@ import { NavLink } from "react-router-dom"
 const SideBar = () => {
   return (
     <div className={styles.sideBarWrapper}>
-      <div className={styles.sideBarSection}>
-        <div className={styles.sideBarItem}>placeholder for army icons</div>
-      </div>
-      <div className={styles.sideBarSection}>
-        <NavLink to={"/products"}>All Products</NavLink>
-        {categories.map((navigation, index) => (
-          <div className={`${styles.navItem} ${styles.sideBarItem}`} key={index}>
-            <NavLink to={`/products/${navigation.value}`}>{navigation.name}</NavLink>
+      <div className={styles.sideBarContent}>
+        <div className={styles.sideBarSection}>
+          <div className={styles.sideBarItem}>placeholder for army icons</div>
+        </div>
+        <div className={styles.sideBarSection}>
+          <NavLink to={"/products"}>All Products</NavLink>
+          {categories.map((navigation, index) => (
+            <div className={`${styles.navItem} ${styles.sideBarItem}`} key={index}>
+              <NavLink to={`/products/${navigation.value}`}>{navigation.name}</NavLink>
+            </div>
+          ))}
+        </div>
+        <div className={styles.sideBarSection}>
+          <div className={styles.sideBarItem}>filter by prices placeholder</div>
+        </div>
+        <div className={styles.sideBarSection}>
+          <div className={styles.iconContainer}>
+            <ShoppingBagIcon size={28}  className={styles.cartIcon} />
+            <UserCircleIcon size={32} className={styles.loginIcon} />
           </div>
-        ))}
-      </div>
-      <div className={styles.sideBarSection}>
-        <div className={styles.sideBarItem}>filter by prices placeholder</div>
-      </div>
-      <div className={styles.sideBarSection}>
-        <div className={styles.iconContainer}>
-          <ShoppingBagIcon size={28}  className={styles.cartIcon} />
-          <UserCircleIcon size={32} className={styles.loginIcon} />
         </div>
       </div>
     </div>
